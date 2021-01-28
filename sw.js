66c210ac812156aa2c52740ac9fff3bf840689fc
yoru-san <cecel31@free.fr>
2021-01-28 23:19:10 +0100

    correction erreur reg undefined

--- sw.js
+++ sw.js
@@ -17,7 +17,6 @@ const files = [
 
 self.addEventListener("push", e => {
   const data = e.data.json();
-  console.log(data);
   self.registration.showNotification(data.title, {
     body: data.body
   });
